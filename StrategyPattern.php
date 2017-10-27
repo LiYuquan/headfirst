dad716746855ff2d39c9e27e042bf07aebec3d55
LiYuquan <liyuquan0914@gmail.com>
2017-10-27 11:31:01 +0800

    鸭子，有的会飞，有的会叫，把飞和叫作为单独的接口，让不同的鸭子调用该接口，根据鸭子种类的不同，选择该接口下不同的实现。

---
+++ StrategyPattern.php
@@ -0,0 +1,171 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lee
+ * Date: 2017/10/27
+ * Time: 10:37
+ * Description: 策略模式
+ */
+
+/***
+ * Interface FlyBehavior
+ */
+interface FlyBehavior
+{
+    /**
+     * @return mixed
+     */
+    public function fly();
+}
+
+/***
+ * Interface QuackBehavior
+ */
+interface QuackBehavior
+{
+    /**
+     * @return mixed
+     */
+    public function quack();
+}
+
+/***
+ * Class FlyWithWings
+ */
+class FlyWithWings implements FlyBehavior
+{
+    /**
+     *
+     */
+    public function fly()
+    {
+        // TODO: Implement fly() method.
+        echo "i can fly";
+    }
+}
+
+/***
+ * Class FlyWithNothing
+ */
+class FlyWithNothing implements FlyBehavior
+{
+    /**
+     *
+     */
+    public function fly()
+    {
+        // TODO: Implement fly() method.
+        echo "i can't fly";
+    }
+}
+
+/***
+ * Class Quack
+ */
+class Quack implements QuackBehavior
+{
+    /**
+     * Quack constructor.
+     */
+    public function quack()
+    {
+        // TODO: Implement quack() method.
+        echo "gua gua gua";
+    }
+}
+
+/***
+ * Class Squeak
+ */
+class Squeak implements QuackBehavior
+{
+    /**
+     *
+     */
+    public function quack()
+    {
+        // TODO: Implement quack() method.
+        echo "zhi zhi zhi";
+    }
+}
+
+/***
+ * Class MuteQuack
+ */
+class MuteQuack implements QuackBehavior
+{
+    /**
+     *
+     */
+    public function quack()
+    {
+        // TODO: Implement quack() method.
+        echo "nothing to say";
+    }
+}
+
+/***
+ * Class Duck
+ */
+class Duck
+{
+    private $_flyBehavior;
+    private $_quackBehavior;
+
+    /**
+     *
+     */
+    public function performQuack()
+    {
+        $this->_quackBehavior->quack();
+    }
+
+    /**
+     *
+     */
+    public function performFly()
+    {
+        $this->_flyBehavior->fly();
+    }
+
+    /**
+     * @param $quackBehavior
+     */
+    public function setQuackBehavior($quackBehavior)
+    {
+        $this->_quackBehavior = $quackBehavior;
+    }
+
+    /**
+     * @param mixed $flyBehavior
+     */
+    public function setFlyBehavior($flyBehavior)
+    {
+        $this->_flyBehavior = $flyBehavior;
+    }
+}
+
+/***
+ * Class MallardDuck
+ */
+class MallardDuck extends Duck
+{
+    /**
+     * MallardDuck constructor.
+     */
+    function __construct()
+    {
+        $this->setQuackBehavior(new MuteQuack());
+        $this->setFlyBehavior(new FlyWithWings());
+    }
+}
+
+/** @var  $m */
+$m = new MallardDuck();
+if (isset($m)) {
+    $m->performQuack();
+}
+if (isset($m)) {
+    $m->performFly();
+}
